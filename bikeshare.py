685943dd2e7c1c01c045aae7ad02f67c0869e3d1
Semilogo Sonaike <semilogosonaike@gmail.com>
2022-01-08 12:10:39 +0100

    refactor: evaluate lower case on input statement lower() function is now found on same line with input statement.

--- bikeshare.py
+++ bikeshare.py
@@ -233,8 +233,8 @@ def main():
         view_data(df)
 
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
+        restart = input('\nWould you like to restart? Enter yes or no.\n').lower()
+        if restart != 'yes':
             break
 
 
